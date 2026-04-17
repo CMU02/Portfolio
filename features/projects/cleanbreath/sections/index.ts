27758a151c3633aa157244c07b2fa284f3dea26d
HyeonJun <hyeonjun1968@naver.com>
2026-04-17 19:42:55 +0900

    refactor: DeepDive 전환으로 불필요해진 구 프로젝트 페이지 삭제
    - CleanBreathPage 및 관련 섹션 컴포넌트 제거
    - PhantomFilePage 제거
    - SubhubPage 제거
    - 각 프로젝트 모두 DeepDivePage로 전환 완료

--- features/projects/cleanbreath/sections/index.ts
+++
@@ -1,4 +0,0 @@
-// CleanBreath 프로젝트 섹션 컴포넌트 모음
-export { ProblemSection } from "./ProblemSection";
-export { SeoResultSection } from "./SeoResultSection";
-export { TroubleShootingSection } from "./TroubleShootingSection";
